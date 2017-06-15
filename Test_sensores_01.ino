82442c53558bb719f69abde0ea847617b0b721b5
Israellh2001 <Israellh2001@hotmail.com>
2017-06-14 22:43:00 -0500

    Nuevo codigo comentado
    Se comento el codigo anterior para darle mejor entendimiento.

--- Test_sensores_01.ino
+++ Test_sensores_01.ino
@@ -1,72 +1,60 @@
-//SENSOR DE TEMPERATURA
+////////////////////VARIABLES DEL SENSOR DE TEMPERATURA/////////////////////
 int tempC;
 int lecTemp;
-int temPin = A9;
-int x=0;  //variable que almacena la lectura de temperatura
-int g=0;  //variable que almacena la humedad
-int f=0; //variable que almacena la temperatura convertida a centigrados 
-const int rele = 52;
-
-//SENSOR DE HUMEDAD
-byte humPin = A8;
-byte humVcc = 33;
-//////////////////////////////////////////////////////////////////////
+int temPin = A9; //pin analogico del sensor 
+int exttemp=0;
+int temp=0;  
+/////////////////RELEVADOR//////////////////////////////////////////////////
+const int rele = 52; //pin digital del relevador 
+//////////////////VARIABLES DEL SENSOR DE HUMEDAD///////////////////////////
+byte humPin = A8; //pin analogico del sensor 
+byte humVcc = 33; //pin digital del sensor 
+int exthumanalog=0;
+int auxhum=0;
+/////////////////////FUNCIONES CONTROLADORAS DE LOS SENSORES////////////////
 void LM35(){
  lecTemp = analogRead(temPin);
- tempC = lecTemp/9.31;
- x=lecTemp;
- f = tempC;
+ tempC = lecTemp/9.31;        //Se convierte la lectura del sensor a grados centigrados
+ temp=lecTemp;
+ exttemp = tempC;
  Serial.println("Sensor de temperatura :");
  Serial.print(lecTemp);
  Serial.print(" C\n");
 }
-//////////////////////////////////////////////////////////////////////
 void YL69(){
  digitalWrite(humVcc, HIGH);
  delay(500);
  int val = analogRead(humPin);
  digitalWrite(humVcc,LOW);
  int sen = 1023-val;
- int humT = ( sen /10);
- g = humT;
+ int humT = ( sen /10);     //Se convierte la lectura del sensor en un porcentaje de 100-0%
+ auxhum= humT;
  Serial.println("Sensor de humedad :");
  Serial.print(humT);
  Serial.print("%\n");
 }
-//////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 void setup() {
-  Serial.begin(9600);
-  //analogReference(INTERNAL);
-  
- //SENSOR DE HUMEDAD
+ //Se declara la comunicacion de los sensores con arduino
   pinMode (humVcc,OUTPUT);
   digitalWrite(humPin,LOW);
-  pinMode(8,OUTPUT);
-  pinMode(10,OUTPUT);
+  pinMode(rele, OUTPUT);
  
  while(!Serial);
  delay(1000);
- pinMode(rele, OUTPUT);
  Serial.begin(9600); 
 }
 
 void loop() {
-  digitalWrite(8,LOW);
-  digitalWrite(10,LOW);
   digitalWrite(rele,LOW);
-//SENSOR DE TEMPERATURA
+//SENSOR DE TEMPERATURA Y SUS RESPECTIVAS CONDICIONALES
   LM35();
-  if(f<=30){
-    digitalWrite(10,HIGH);
-  }
   if(x>60){
    digitalWrite (rele, HIGH);
   }
-//SENSOR DE HUMEDAD
+//SENSOR DE HUMEDAD Y SUS RESPECTIVAS CONDICIONALES 
   YL69();
-  if (g>=40){
-  digitalWrite(8,HIGH);
-  }
+  
   delay(1000);
 }
